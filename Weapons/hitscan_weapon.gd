1e1a79aa7975a0c9864558edb7bf2b6ae0482274
DTNevaeha <DTNevaeha@gmail.com>
2024-05-30 21:01:38 -0500

    Added hit sparks

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
 @export var fire_rate := 14.0
+@export var muzzle_flash: GPUParticles3D
 @export var recoil := 0.05
+@export var sparks: PackedScene
 @export var weapon_damage := 15
 @export var weapon_mesh: Node3D
-@export var muzzle_flash: GPUParticles3D
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
@@ -26,3 +27,6 @@ func shoot() -> void:
 	weapon_mesh.position.z += recoil
 	if collider is Enemy:
 		collider.hitpoints -= weapon_damage
+	var spark = sparks.instantiate()
+	add_child(spark)
+	spark.global_position = ray_cast_3d.get_collision_point()
